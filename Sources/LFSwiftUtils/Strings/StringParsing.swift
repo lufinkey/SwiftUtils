e93d4dbf78060b7d7b39d8a208b4d5d84dd33596
Luis Finke <luisfinke@gmail.com>
2025-05-18 13:21:34 -0400

    anyLine(where:) and forEachLine available in substring

--- Sources/LFSwiftUtils/Strings/StringParsing.swift
+++ Sources/LFSwiftUtils/Strings/StringParsing.swift
@@ -13,39 +13,63 @@ extension String {
 	public typealias AnyLineWhereAsyncHandler = (_ line: Substring, _ lineEnd: Regex<Substring>.Match?) async throws -> Bool
 	
 	@available(macOS 13.0, *)
-	public func anyLine(where handler: AnyLineWhereHandler) rethrows {
-		var startIndex = self.startIndex
-		while startIndex != self.endIndex {
-			let newLineMatch = self[startIndex...].firstMatch(of: /\r\n|\n|\r/)
-			let line = self[startIndex..<(newLineMatch?.startIndex ?? self.endIndex)]
-			if try handler(line,newLineMatch) {
-				return
-			}
-			startIndex = newLineMatch?.endIndex ?? self.endIndex
-		}
-	}
-	
-	@available(macOS 13.0, *)
-	public func anyLine(where handler: AnyLineWhereAsyncHandler) async rethrows {
-		var startIndex = self.startIndex
-		while startIndex != self.endIndex {
-			let newLineMatch = self[startIndex...].firstMatch(of: /\r\n|\n|\r/)
-			let line = self[startIndex..<(newLineMatch?.startIndex ?? self.endIndex)]
-			if try await handler(line,newLineMatch) {
-				return
-			}
-			startIndex = newLineMatch?.endIndex ?? self.endIndex
-		}
-	}
-	
-	@available(macOS 13.0, *)
 	public typealias ForEachLineHandler = (_ line: Substring, _ lineEnd: Regex<Substring>.Match?) throws -> Void
 	
 	@available(macOS 13.0, *)
 	public typealias ForEachLineAsyncHandler = (_ line: Substring, _ lineEnd: Regex<Substring>.Match?) async throws -> Void
 	
 	@available(macOS 13.0, *)
-	public func forEachLine(_ handler: ForEachLineHandler) rethrows {
+	public func anyLine(where handler: AnyLineWhereHandler) rethrows -> Bool {
+		return try self[...].anyLine(where: handler)
+	}
+	
+	@available(macOS 13.0, *)
+	public func anyLine(where handler: AnyLineWhereAsyncHandler) async rethrows -> Bool {
+		return try await self[...].anyLine(where: handler)
+	}
+	
+	@available(macOS 13.0, *)
+	public func forEachLine(where handler: ForEachLineHandler) rethrows {
+		try self[...].forEachLine(handler)
+	}
+	
+	@available(macOS 13.0, *)
+	public func anyLine(where handler: ForEachLineAsyncHandler) async rethrows {
+		try await self[...].forEachLine(handler)
+	}
+}
+
+extension Substring {
+	@available(macOS 13.0, *)
+	public func anyLine(where handler: String.AnyLineWhereHandler) rethrows -> Bool {
+		var startIndex = self.startIndex
+		while startIndex != self.endIndex {
+			let newLineMatch = self[startIndex...].firstMatch(of: /\r\n|\n|\r/)
+			let line = self[startIndex..<(newLineMatch?.startIndex ?? self.endIndex)]
+			if try handler(line,newLineMatch) {
+				return true
+			}
+			startIndex = newLineMatch?.endIndex ?? self.endIndex
+		}
+		return false
+	}
+	
+	@available(macOS 13.0, *)
+	public func anyLine(where handler: String.AnyLineWhereAsyncHandler) async rethrows -> Bool {
+		var startIndex = self.startIndex
+		while startIndex != self.endIndex {
+			let newLineMatch = self[startIndex...].firstMatch(of: /\r\n|\n|\r/)
+			let line = self[startIndex..<(newLineMatch?.startIndex ?? self.endIndex)]
+			if try await handler(line,newLineMatch) {
+				return true
+			}
+			startIndex = newLineMatch?.endIndex ?? self.endIndex
+		}
+		return false
+	}
+	
+	@available(macOS 13.0, *)
+	public func forEachLine(_ handler: String.ForEachLineHandler) rethrows {
 		var startIndex = self.startIndex
 		while startIndex != self.endIndex {
 			let newLineMatch = self[startIndex...].firstMatch(of: /\r\n|\n|\r/)
@@ -56,7 +80,7 @@ extension String {
 	}
 	
 	@available(macOS 13.0, *)
-	public func forEachLine(_ handler: ForEachLineAsyncHandler) async rethrows {
+	public func forEachLine(_ handler: String.ForEachLineAsyncHandler) async rethrows {
 		var startIndex = self.startIndex
 		while startIndex != self.endIndex {
 			let newLineMatch = self[startIndex...].firstMatch(of: /\r\n|\n|\r/)
